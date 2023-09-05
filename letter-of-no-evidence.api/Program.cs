f4bd43887e9f7cb39a12fbb25b3f9e9191ae4672
Gnanesh V <msdn4gnanesh@gmail.com>
2023-09-05 10:25:27 +0100

    Updated the NLog

--- letter-of-no-evidence.api/Program.cs
+++ letter-of-no-evidence.api/Program.cs
@@ -1,10 +1,10 @@
 using Amazon.Extensions.NETCore.Setup;
-using Amazon.SimpleSystemsManagement;
 using letter_of_no_evidence.api.Logging;
 using letter_of_no_evidence.api.Service;
 using letter_of_no_evidence.data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using NLog;
 using NLog.Web;
 
 namespace letter_of_no_evidence.api
@@ -13,55 +13,68 @@ namespace letter_of_no_evidence.api
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
-            builder.Services.AddControllers();
-
-            // Get the AWS profile information from configuration providers
-            AWSOptions awsOptions = builder.Configuration.GetAWSOptions();
-            // Configure AWS service clients to use these credentials
-            builder.Services.AddDefaultAWSOptions(awsOptions);
-            builder.Services.AddDataProtection().PersistKeysToAWSSystemsManager("/LONE-API/DataProtection");
-
-            // Add NLoging to the container.
-            builder.Logging.ClearProviders();
-            builder.Logging.AddConsole();
-            builder.Logging.SetMinimumLevel(LogLevel.Warning);
-            builder.Host.UseNLog();
-
-            var logger = NLogHelper.ConfigureLogger();
-
-            builder.Services.AddDbContext<LONEDBContext>(opt =>
-                     opt.UseSqlServer(Environment.GetEnvironmentVariable("LONEConnection"))
-                        .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
-
-            // Add services to the container.
-            builder.Services.AddScoped<IRequestService, RequestService>();
-            builder.Services.AddScoped<IPaymentService, PaymentService>();
-
-
-
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(c =>
+            try
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Letter of no evidence-api", Version = "v1" });
-            });
+                var builder = WebApplication.CreateBuilder(args);
 
-            var app = builder.Build();
+                builder.Services.AddControllers();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Letter of no evidence-api v1"));
+                // Add NLoging to the container.
+                builder.Logging.ClearProviders();
+                builder.Logging.AddConsole();
+                builder.Host.UseNLog();
+
+                // Get the AWS profile information from configuration providers
+                AWSOptions awsOptions = builder.Configuration.GetAWSOptions();
+                // Configure AWS service clients to use these credentials
+                builder.Services.AddDefaultAWSOptions(awsOptions);
+                builder.Services.AddDataProtection().PersistKeysToAWSSystemsManager("/LONE-API/DataProtection");
+
+                builder.Services.AddDbContext<LONEDBContext>(opt =>
+                         opt.UseSqlServer(Environment.GetEnvironmentVariable("LONEConnection"))
+                            .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
+
+                // Add services to the container.
+                builder.Services.AddScoped<IRequestService, RequestService>();
+                builder.Services.AddScoped<IPaymentService, PaymentService>();
+
+
+
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Letter of no evidence-api", Version = "v1" });
+                });
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                    app.UseSwagger();
+                    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Letter of no evidence-api v1"));
+                }
+                app.ConfigureExceptionHandler(logger);
+
+                app.UseRouting();
+                app.MapControllers();
+                app.Run();
             }
-            app.ConfigureExceptionHandler(logger);
-
-            app.UseRouting();
-            app.MapControllers();
-            app.Run();
+            catch (Exception exception)
+            {
+                // NLog: catch setup errors
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
     }
 }
--- letter-of-no-evidence.web/Program.cs
+++ letter-of-no-evidence.web/Program.cs
@@ -5,6 +5,7 @@ using letter_of_no_evidence.web.Helper;
 using letter_of_no_evidence.web.Logging;
 using letter_of_no_evidence.web.Service;
 using Microsoft.AspNetCore.Mvc;
+using NLog;
 using NLog.Web;
 using System.Net.Http.Headers;
 
@@ -14,128 +15,139 @@ namespace letter_of_no_evidence.web
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddHttpContextAccessor();
-
-            builder.Services.AddControllers();
-
-            builder.Services.AddSession(options =>
+            try
             {
-                options.Cookie.Name = "LONE.Session";
-                options.Cookie.IsEssential = true;
-            });
+                var builder = WebApplication.CreateBuilder(args);
 
-            // Get the AWS profile information from configuration providers
-            AWSOptions awsOptions = builder.Configuration.GetAWSOptions();
-            // Configure AWS service clients to use these credentials
-            builder.Services.AddDefaultAWSOptions(awsOptions);
-            builder.Services.AddDataProtection().PersistKeysToAWSSystemsManager("/LONE-WEB/DataProtection");
-            builder.Services.AddAWSService<IAmazonSimpleEmailService>();
-            builder.Services.AddScoped<IEmailService, EmailService>();
+                // Add services to the container.
+                builder.Services.AddControllersWithViews();
+                builder.Services.AddHttpContextAccessor();
 
-            // Add NLoging to the container.
-            builder.Logging.ClearProviders();
-            builder.Logging.AddConsole();
-            builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Warning);
-            builder.Host.UseNLog();
+                // Add NLoging to the container.
+                builder.Logging.ClearProviders();
+                builder.Logging.AddConsole();
+                builder.Host.UseNLog();
 
-            var logger = NLogHelper.ConfigureLogger();
-
-            builder.Services.AddHttpClient<IPaymentService, PaymentService>(c =>
-            {
-                c.BaseAddress = new Uri(Environment.GetEnvironmentVariable("GOV_PAY_URL"));
-                c.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Environment.GetEnvironmentVariable("GOV_PAY_API_KEY"));
-                c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            });
-
-            builder.Services.AddHttpClient<IRequestService, RequestService>(c =>
-            {
-                c.BaseAddress = new Uri(Environment.GetEnvironmentVariable("LONE_WebApi_URL"));
-                c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            });
-
-            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-            builder.Services.AddMvc(options =>
-            {
-                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-            });
-
-            var app = builder.Build();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/error");
-            }
-            app.ConfigureExceptionHandler(logger);
-            app.RegisterTNACookieConsent();
-            app.UseSecurityHeaderMiddleware();
-            app.UseRouting();
-
-            var rootPath = Environment.GetEnvironmentVariable("LONE_Root_Path");
-            if (!string.IsNullOrWhiteSpace(rootPath))
-            {
-                app.Use((context, next) =>
+                builder.Services.AddSession(options =>
                 {
-                    context.Request.PathBase = new PathString(rootPath);
-                    return next();
+                    options.Cookie.Name = "LONE.Session";
+                    options.Cookie.IsEssential = true;
                 });
+
+                // Get the AWS profile information from configuration providers
+                AWSOptions awsOptions = builder.Configuration.GetAWSOptions();
+                // Configure AWS service clients to use these credentials
+                builder.Services.AddDefaultAWSOptions(awsOptions);
+                builder.Services.AddDataProtection().PersistKeysToAWSSystemsManager("/LONE-WEB/DataProtection");
+                builder.Services.AddAWSService<IAmazonSimpleEmailService>();
+                builder.Services.AddScoped<IEmailService, EmailService>();
+
+                builder.Services.AddHttpClient<IPaymentService, PaymentService>(c =>
+                {
+                    c.BaseAddress = new Uri(Environment.GetEnvironmentVariable("GOV_PAY_URL"));
+                    c.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Environment.GetEnvironmentVariable("GOV_PAY_API_KEY"));
+                    c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                });
+
+                builder.Services.AddHttpClient<IRequestService, RequestService>(c =>
+                {
+                    c.BaseAddress = new Uri(Environment.GetEnvironmentVariable("LONE_WebApi_URL"));
+                    c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                });
+
+                builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+                builder.Services.AddMvc(options =>
+                {
+                    options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+                });
+
+                var app = builder.Build();
+                app.ConfigureExceptionHandler(logger);
+
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                }
+                else
+                {
+                    app.UseExceptionHandler("/error");
+                }
+                app.RegisterTNACookieConsent();
+                app.UseSecurityHeaderMiddleware();
+                app.UseRouting();
+
+                var rootPath = Environment.GetEnvironmentVariable("LONE_Root_Path");
+                if (!string.IsNullOrWhiteSpace(rootPath))
+                {
+                    app.Use((context, next) =>
+                    {
+                        context.Request.PathBase = new PathString(rootPath);
+                        return next();
+                    });
+                }
+                app.UseStaticFiles();
+                app.UseSession();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "",
+                        new { controller = "Home", action = "Index" });
+
+                    endpoints.MapControllerRoute(
+                        name: "subject-details",
+                        pattern: "subject-details",
+                        new { controller = "Request", action = "SubjectDetails" });
+
+                    endpoints.MapControllerRoute(
+                        name: "contact-details",
+                        pattern: "contact-details",
+                        new { controller = "Request", action = "ContactDetails" });
+
+                    endpoints.MapControllerRoute(
+                        name: "postal-details",
+                        pattern: "postal-details",
+                        new { controller = "Request", action = "PostalDetails" });
+
+                    endpoints.MapControllerRoute(
+                        name: "contact-email",
+                        pattern: "contact-email",
+                        new { controller = "Request", action = "ContactEmail" });
+
+                    endpoints.MapControllerRoute(
+                        name: "request-summary",
+                        pattern: "request-summary",
+                        new { controller = "Request", action = "RequestSummary" });
+
+                    endpoints.MapControllerRoute(
+                        name: "request-receipt",
+                        pattern: "request-receipt/{requestNumber}",
+                        new { controller = "Request", action = "RequestReceipt" });
+
+                    endpoints.MapControllerRoute(
+                        name: "try-again",
+                        pattern: "try-again/{requestNumber}",
+                        new { controller = "Request", action = "TryAgain" });
+
+                });
+
+                app.Run();
             }
-            app.UseStaticFiles();
-            app.UseSession();
-
-            app.UseEndpoints(endpoints =>
+            catch (Exception exception)
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "",
-                    new { controller = "Home", action = "Index" });
-
-                endpoints.MapControllerRoute(
-                    name: "subject-details",
-                    pattern: "subject-details",
-                    new { controller = "Request", action = "SubjectDetails" });
-
-                endpoints.MapControllerRoute(
-                    name: "contact-details",
-                    pattern: "contact-details",
-                    new { controller = "Request", action = "ContactDetails" });
-
-                endpoints.MapControllerRoute(
-                    name: "postal-details",
-                    pattern: "postal-details",
-                    new { controller = "Request", action = "PostalDetails" });
-
-                endpoints.MapControllerRoute(
-                    name: "contact-email",
-                    pattern: "contact-email",
-                    new { controller = "Request", action = "ContactEmail" });
-
-                endpoints.MapControllerRoute(
-                    name: "request-summary",
-                    pattern: "request-summary",
-                    new { controller = "Request", action = "RequestSummary" });
-
-                endpoints.MapControllerRoute(
-                    name: "request-receipt",
-                    pattern: "request-receipt/{requestNumber}",
-                    new { controller = "Request", action = "RequestReceipt" });
-
-                endpoints.MapControllerRoute(
-                    name: "try-again",
-                    pattern: "try-again/{requestNumber}",
-                    new { controller = "Request", action = "TryAgain" });
-
-            });
-
-            app.Run();
+                // NLog: catch setup errors
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
     }
 }
