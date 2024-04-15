21620cbc91e5c1eaa09039fe1afd49e3909577b8
Gnanesh V <msdn4gnanesh@gmail.com>
2024-04-15 15:14:56 +0100

    Updated the current culture to en-GB

--- letter-of-no-evidence.web/Helper/CostExtension.cs
+++ letter-of-no-evidence.web/Helper/CostExtension.cs
@@ -4,21 +4,22 @@ namespace letter_of_no_evidence.web.Helper
 {
     public static class CostExtension
     {
+        private static CultureInfo cultureInfo = new CultureInfo("en-GB");
         public static string LoneCost()
         {
             var amount = Decimal.Divide(int.Parse(Environment.GetEnvironmentVariable("LONE_Amount")), 100);
-            return amount.ToString("C", CultureInfo.CurrentCulture);
+            return amount.ToString("C", cultureInfo);
         }
 
         public static string FormatedCost(decimal cost)
         {
-            return cost.ToString("C", CultureInfo.CurrentCulture);
+            return cost.ToString("C", cultureInfo);
         }
 
         public static string FormatedTotalCost(decimal cost)
         {
             var amount = Decimal.Divide(int.Parse(Environment.GetEnvironmentVariable("LONE_Amount")), 100) + cost;
-            return amount.ToString("C", CultureInfo.CurrentCulture);
+            return amount.ToString("C", cultureInfo);
         }
     }
 }
